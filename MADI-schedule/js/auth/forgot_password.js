9b865ecbe8919ac29f4df5672cec4bc2149bdcc5
zzzuzuzzz <142272453+zzzuzuzzz@users.noreply.github.com>
2023-11-30 23:02:53 +0300

    Add forgot password, but only html

---
+++ MADI-schedule/js/auth/forgot_password.js
@@ -0,0 +1,9 @@
+let btn_back = document.querySelector('.btn_main_containers_container_back')
+btn_back.onclick = function () {
+    document.location.href = '../../html/auth/auth.html'
+}
+
+let btn_forgot = document.querySelector('.btn_main_containers_container_continue')
+btn_forgot.onclick = function () {
+    document.location.href = '../../html/auth/auth.html'
+}
