f35f6eac806f83004d83e3537c3060cac19eaebc
zzzuzuzzz <142272453+zzzuzuzzz@users.noreply.github.com>
2023-12-02 08:19:27 +0300

    add js's code in registers html + start write forgot pasword's code

--- MADI-schedule/js/auth/register_1_step.js
+++ MADI-schedule/js/auth/register_1_step.js
@@ -1,9 +1,18 @@
+let result = document.querySelector('.opt_main_containers_container_opt_one')
+
 let btn_back = document.querySelector('.btn_main_containers_container_back')
 btn_back.onclick = function () {
     document.location.href = '../../html/auth/auth.html'
 }
 
-function next_doc(){document.location.href = '../../html/auth/register_teacher_step.html'}
 
-let form = document.querySelector('.main_containers_container')
-form.addEventListener('submit', next_doc)
+let btn_enter = document.querySelector('.btn_main_containers_container_continue')
+btn_enter.onclick = function () {
+    if (result.value === 'teacher') {
+        document.location.href = '../../html/auth/register_teacher_step.html'
+    } else if (result.value === 'student') {
+        document.location.href = '../../html/auth/register_student_step.html'
+    } else {
+        alert("Вы должны выбрать один из вариантов")
+    }
+}
--- MADI-schedule/js/auth/register_student_step.js
+++ MADI-schedule/js/auth/register_student_step.js
@@ -3,7 +3,22 @@ btn_back.onclick = function () {
     document.location.href = '../../html/auth/register_1_step.html'
 }
 
-function next_doc(){document.location.href = '../../html/auth/register_final_step.html'}
+let result = document.querySelector('.opt_main_containers_container_opt_one')
 
-let form = document.querySelector('.main_containers_container')
-form.addEventListener('submit', next_doc)
+let btn_enter = document.querySelector('.btn_main_containers_container_continue')
+btn_enter.onclick = function () {
+    if (result.value === 'unvalue') {
+        alert('Вы должны выбрать один из вариантов')
+    } else {
+        document.location.href = '../../html/auth/register_final_step.html'
+    }
+}
+
+
+
+
+
+// function next_doc(){document.location.href = '../../html/auth/register_final_step.html'}
+//
+// let form = document.querySelector('.main_containers_container')
+// form.addEventListener('submit', next_doc)
