08cc184737d29c924397481a1cc18ef6eb49727b
zzzuzuzzz <142272453+zzzuzuzzz@users.noreply.github.com>
2023-12-02 14:45:18 +0300

    add js's code in forgot's htmls

--- MADI-schedule/js/auth/forgot_2_step.js
+++ MADI-schedule/js/auth/forgot_2_step.js
@@ -1,9 +1,4 @@
-let btn_back = document.querySelector('.btn_main_containers_container_back')
-btn_back.onclick = function () {
-    document.location.href = '../../html/auth/auth.html'
-}
-
 let btn_forgot = document.querySelector('.btn_main_containers_container_continue')
 btn_forgot.onclick = function () {
-    document.location.href = '../../html/auth/forgot_2_step.html'
+    document.location.href = '../../html/auth/auth.html'
 }
--- MADI-schedule/js/auth/forgot_password.js
+++ MADI-schedule/js/auth/forgot_password.js
@@ -3,7 +3,13 @@ btn_back.onclick = function () {
     document.location.href = '../../html/auth/auth.html'
 }
 
+let result = document.querySelector('.ipt_main_containers_container_email')
+
 let btn_forgot = document.querySelector('.btn_main_containers_container_continue')
 btn_forgot.onclick = function () {
-    document.location.href = '../../html/auth/forgot_2_step.html'
+    if (result.value === '') {
+        alert('Введите почту')
+    } else {
+        document.location.href = '../../html/auth/forgot_2_step.html'
+    }
 }
