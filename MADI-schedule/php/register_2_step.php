46f1fed89cb5c77907136a16391c851f04fea537
zzzuzuzzz <142272453+zzzuzuzzz@users.noreply.github.com>
2023-12-05 16:18:24 +0300

    Fix php_2_step

--- MADI-schedule/php/register_2_step.php
+++ MADI-schedule/php/register_2_step.php
@@ -2,9 +2,10 @@
 $name = $_POST['name'];
 $select = $_POST['select'];
 
-if (trim($name) === '') {
-    echo 'Вы не ввели свое ФИО';
-} else if (trim($select) === '') {
+//if (trim($name) === '') {
+//    echo 'Вы не ввели свое ФИО';
+//}
+if (trim($select) === '') {
     echo 'Вы не выбрали вариант из выпадающего списка';
 } else {
     $mysql = new mysqli('192.168.1.74', 'test', 'test', 'test');
