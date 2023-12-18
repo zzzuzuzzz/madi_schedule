abbcc66d1f4450cd06ff9b2edc11316193288bb4
zzzuzuzzz <142272453+zzzuzuzzz@users.noreply.github.com>
2023-12-18 15:43:35 +0300

    -

--- MADI-schedule/php/register_1_step.php
+++ MADI-schedule/php/register_1_step.php
@@ -10,40 +10,24 @@
         echo 'Недопустимая длина пароля';
     } else if ((trim($password)) !== (trim($password_two))) {
         echo 'Пароли не совпадают';
+    } else if ((trim($select)) === '') {
+        echo 'Вы не выбрали вариант из выпадающего списка';
     } else {
 
+        $mysql = new mysqli('192.168.1.74', 'test', 'test', 'test');
+        if ($mysql->connect_error) {
+            echo 'Error Number: '.$mysql->connect_errno.'<br>';
+            echo 'Error: '.$mysql->connect_error;
+            exit();
+        } else {
+            $mysql -> query("INSERT INTO `main_table` (`email`, `password`, `work`, `name`, `class`) VALUES($mail, $password, $select, $mail, $mail)");
+        }
+        $mysql->close();
         if ($select === 'teacher') {
-            $mysql = new mysqli('192.168.1.74', 'test', 'test', 'test');
-            if ($mysql->connect_error) {
-                echo 'Error Number: '.$mysql->connect_errno.'<br>';
-                echo 'Error: '.$mysql->connect_error;
-                exit();
-            }
-            else {
-                $mysql -> query("INSERT INTO `main_table` (`email`, `password`, `work`, `name`, `class`) VALUES($mail, $password, $select, null, null)");
-            }
-            $mysql->close();
             header('Location: ../html/auth/register_teacher_step.html');
             exit();
-
-
         } else if ($select === 'student') {
-            $mysql = new mysqli('192.168.1.74', 'test', 'test', 'test');
-            if ($mysql->connect_error) {
-                echo 'Error Number: '.$mysql->connect_errno.'<br>';
-                echo 'Error: '.$mysql->connect_error;
-                exit();
-            }
-            else {
-
-                $mysql -> query("INSERT INTO `main_table` (`email`, `password`, `work`, `name`, `class`) VALUES($mail, $password, $select, null, null)");
-            }
-            $mysql->close();
             header('Location: ../html/auth/register_student_step.html');
             exit();
-        }
-
-        else {
-            echo 'Вы не выбрали вариант из выпадающего списка';
         }
     }
