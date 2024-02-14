465e0fc23b95cb19d751c1a3f5922c481399101f
zzzuzuzzz <142272453+zzzuzuzzz@users.noreply.github.com>
2024-02-14 16:41:51 +0300

    -

--- test.js
+++
@@ -1,7 +0,0 @@
-const axios = require('axios');
-const cheerio = require('cheerio');
-
-axios.get('https://raspisanie.madi.ru/tplan/r/?task=7').then(html => {
-    const $ = html.data
-    console.log($)
-})
